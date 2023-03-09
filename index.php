cf2d1a1bb0534b372d166a403f8700f12b68aa9d
Nahid Ghafari <126081955+nahid-ghafari@users.noreply.github.com>
2023-03-09 20:11:30 +0330

    continuation of the prj 3-2 (bootstrap)

--- index.php
+++ index.php
@@ -4,62 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>lesson 3</title>
 </head>
 <body>
-    <!-- exercise array -->
-    <?php 
-    $books = [
-       [
-            "title" => " The first book",
-            "Author" =>" The first author",
-            "Publish-date" =>" 1395",
-            "price" =>" 100000",
-
-
-       ],
-       [
-            "title" => " The second book",
-            "Author" =>" The second author",
-            "Publish-date" =>" 1399",
-            "price" =>" 200000",
-
-
-       ],
-       [
-            "title" => " The third book",
-            "Author" =>" The third author",
-            "Publish-date" =>" 1396",
-            "price" =>" 300000",
-
-       ],
-
+    <!-- exercise array... -->
+    <?php
+    $books= [
+        [
+            "title" => "The first book",
+            "Author" => "The first Author",
+            "publish-date" => "1395",
+            "price" => "100000"
+        ],
+        [
+            "title" => "The second book",
+            "Author" => "The second Author",
+            "publish-date" => "1399",
+            "price" => "200000"
+        ],
+        [
+            
+            "title" => "The third book",
+            "Author" => "The third Author",
+            "publish-date" => "1401",
+            "price" => "350000"
+        ]
     ];
-    var_dump($books);
-    //die(); it means the end .stop
+    // var_dump($books);
+    // die();
     ?>
-    <h1> This is the third session coding</h1>
-    <h3>Books list :</h3>
-    <!-- <table>
-        <?php foreach($books as $key => $value){ ?>
-            <tr>
-                <td>
-                    <?= $key ?>
-                    <?php var_dump($value) ?>
-
-                </td>
-            </tr>
-
-        <?php }?>   
-    </table> -->
-    <table>
-         <?php foreach ($books as $value){ ?>
-             <tr>
-                  <td><?php var_dump($value)?> </td>
-             </tr>
-         <?php }?>
-    </table>    
-        
-
+    <!-- <h1>This is the third session coding</h1> -->
+    <h3>Book List :</h3>
+    <table class="table table-bordered table-dark">
+        <thead>
+            <th> Title </th>
+            <th> Author </th>
+            <th> Publish Date </th>
+            <th> Price </th>
+        </thead>
+        <tbody>
+            <?php foreach($books as $book){ ?>
+                <tr>
+                    <?php foreach($book as $key => $value){ ?>
+                    <td> <?= $value ?> </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </body>
 </html>

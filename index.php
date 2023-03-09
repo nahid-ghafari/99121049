8725499be7810a4b695cb3bedc479a23b09c0f4c
Nahid Ghafari <126081955+nahid-ghafari@users.noreply.github.com>
2023-03-09 16:57:35 +0330

    1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $message="hello world 'this is string'";
+    $name="Nahid Ghafari";
+    echo('hello world');
+    ?>
+    <h1>this is the first session coding</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <h3>Hello<?php echo($name) ?></h3>
+</body>
+</html>

806b85e0fda24b59571a8acf096d7cdedbd1f3e1
Nahid Ghafari <126081955+nahid-ghafari@users.noreply.github.com>
2023-03-09 22:09:26 +0330

    separate and made show php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Lesson 1</title>
-</head>
-<body>
-    <?php
+<?php
     $books = [
         [
             'title' => 'The first book',
@@ -54,27 +44,8 @@
         }
         return false;
 
-    })
+    });
+    require("index.show.php");
 
-    ?>
-    <h3>Books list</h3>
-    <table class="table table-bordered table-dark">
-        <thead>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Publish Date</th>
-            <th>Price</th>
-        </thead>
-        <tbody>
-        <?php foreach($filteredbook as $book): ?>
-            <tr>
-                <?php foreach($book as $key=>$value): ?>
-                    <td><?= $value ?></td>
-                <?php endforeach ?>    
-            </tr>    
-        
-        <?php endforeach ?>
-        </tbody>
-    </table>
-</body>
-</html>
+?>
+    
